d816d0721e704ffce279f6ba6d043b4b097b82d2
RomanoDekkers <romanodekkers04@gmail.com>
2022-12-06 11:47:36 +0100

    Accountbeheer Read en Delete klaar

--- accountbeheer.php
+++ accountbeheer.php
@@ -3,6 +3,23 @@
     include './sjabloon/indeling.php';
     include './connectie/conn.php';
     include './configs/functions.php';
+
+    if (isset($_POST)) {
+        if (isset($_POST['AccountVerwijderen'])) {
+            $sql = 'DELETE FROM `gebruikers` WHERE `ID` = ' .$_SESSION['ID'];
+
+            $qry = $conn -> query($sql);
+            if($qry){
+                session_destroy();
+                echo "<script type='text/javascript'> window.location.href='registreren.php'</script>";
+            }
+            else
+            {
+                echo "Iets ging er mis!<BR>";
+                echo "Error Beschrijving: ", $conn -> error;
+            }
+        }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -15,17 +32,32 @@
 </head>
 <body>
 <div class="container mt-3">
-    <form action="accountbeheer.php">
+    <form action="accountbeheer.php" method="POST">
         <div class="row">
             <div class="col">
-                <input type="text" name="Voornaam" class="form-control" value="<?php echo $_SESSION['Voornaam']; ?>">
+                <input type="text" name="Voornaam" class="form-control" value="<?php echo $_SESSION['Voornaam']; ?>" disabled>
             </div>
             <div class="col">
-                <input type="text" name="Achternaam" class="form-control" value="<?php echo $_SESSION['Achternaam']; ?>">
+                <input type="text" name="Achternaam" class="form-control" value="<?php echo $_SESSION['Achternaam']; ?>" disabled>
             </div>
-        </div>
-        <div class="row">
-            
+        </div>  <br />
+            <div>
+                <input type="text" name="E-mail" class="form-control" value="<?php echo $_SESSION['E-mail']; ?>" disabled>
+        </div>  <br />
+            <div>
+                <input type="text" name="Adres" class="form-control" value="<?php echo $_SESSION['Adres']; ?>" disabled>
+        </div>  <br />
+            <div>
+                <input type="text" name="Telefoonnummer" class="form-control" value="<?php echo $_SESSION['Telefoonnummer']; ?>" disabled>
+         </div> <br />
+            <div>
+                <input type="password" name="Wachtwoord" class="form-control" value="<?php echo md5($_SESSION['Wachtwoord']); ?>" disabled>
+        </div>  <br />
+            <div>
+            <input type="submit" class="btn btn-warning" id="btnSubmit" value="Edit account" name="EditConfirm" onclick="return confirm('Wilt u definitief uw account bewerken? U zal worden uitgelogd.')">
+            <input type="submit" class="btn btn-danger" id="btnSubmit" value="Delete account" name="AccountVerwijderen" onclick="return confirm('Wilt u definitief uw account verwijderen?')">
+            <input type="submit" class="btn btn-info" id="btnSubmit" value="Logout" name="LogoutConfirm" onclick="return confirm('Wilt u uitloggen?')">
+            </div>
         </div>
     </form>
 </div>

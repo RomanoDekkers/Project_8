a3224df92ba2fbf0e6f2b5c4da3ed7eec217cc86
olivier <olivier.etman@edu-kw1c.nl>
2022-12-06 09:24:10 +0100

    homepage

--- sjabloon/indeling.php
+++ sjabloon/indeling.php
@@ -16,6 +16,9 @@ if(isset($_POST)){
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+<link rel = "icon" href = 
+"logo.jpg" 
+        type = "image/x-icon">
 <style>
 
 
@@ -25,12 +28,11 @@ if(isset($_POST)){
 <body>
     <header class="p-3 mb-3 border-bottom">
     <div class="container">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
-          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
+      <div class="d-flex flex-wrap align-items-left justify-content-center justify-content-lg-start">
+        <a href="/" class="d-flex align-items-left mb-2 mb-lg-0 text-dark text-decoration-none">
+        <img src="logo.jpg" alt="logo" width="50" height="50">
         </a>
-
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-left mb-md-0">
           <li><a href="index.php" class="nav-link px-2 link-secondary">Home</a></li>
           <li><a href="Wedstrijd.php" class="nav-link px-2 link-dark">Wedstrijd</a></li>
           <li><a href="Ledenlijst.php" class="nav-link px-2 link-dark">Ledenlijst</a></li>

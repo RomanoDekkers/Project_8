2556390e62d0f6accce5ae37ee6e8522c0bf797d
olivier <olivier.etman@edu-kw1c.nl>
2022-12-07 10:05:32 +0100

    contact

--- contact.php
+++ contact.php
@@ -3,5 +3,33 @@
     include './sjabloon/indeling.php';
     include './connectie/conn.php';
     include './configs/functions.php';
-    
+
+    if(isset($_POST['submit']))
+    {
+        $email = $_POST['email'];
+        $Onderwerp = $_POST['Onderwerp'];
+        $Bericht = $_POST['Bericht'];
+         mail("schaakclub.deblauweloper@gmail.com", $Onderwerp,
+        $Bericht. $email);
+    }
+
     ?>
+<div class="container">
+    <form action="" method="post" enctype="text/plain">
+        <div class="mb-3">
+            <label  class="form-label">Uw email address</label>
+            <input type="email" class="form-control" placeholder="jou email" name="email">
+        </div>
+        <div class="mb-3">
+            <label class="form-label" >Onderwerp</label>
+            <input type="text" name="Onderwerp" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label  class="form-label" >Bericht</label>
+            <textarea class="form-control"  name="Bericht" class="form-control" rows="3"></textarea>
+        </div>
+        <div class="mb-3">
+        <input class="btn btn-success" type="submit" value="Verstuur">
+        </div>
+    </form>
+</div>
